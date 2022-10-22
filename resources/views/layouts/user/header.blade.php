250b0b4e60906eaa213c6f641b4b90b5de126116
Shintya77 <shintyarahma77@gmail.com>
2022-10-22 15:37:05 -0700

    pasang dan pecah template user

---
+++ resources/views/layouts/user/header.blade.php
@@ -0,0 +1,55 @@
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope-fill"></i><a href="mailto:shintyarahma77@gmail.com">email panti</a>
+        <i class="bi bi-phone-fill phone-icon"></i> nomor panti
+      </div>
+      {{-- <div class="cta d-none d-md-block">
+        <a href="#about" class="scrollto">Get Started</a>
+      </div> --}}
+    </div>
+  </div>
+  
+  
+  <header id="header" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="{{url('/beranda')}}">PANTI ASUHAN PUTRI ‘AISYIYAH</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href=index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li class="dropdown"><a href="#"><span>Profil</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Sejarah</a></li>
+              <li><a href="#">Visi & Misi</a></li>
+              <li><a href="#">Tujuan</a></li>
+              <li><a href="#">Kegiatan</a></li>
+              <li><a href="#">Struktur Kepengurusan</a></li>
+              {{-- <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li> --}}
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#services">Galeri</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Kegiatan</a></li>
+          <li><a class="nav-link scrollto" href="#team">Donasi</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Pesan Kue</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Login</a></li>
+         
+        
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('fitur.user.beranda');
 });
+Route::get('/beranda',[BerandaController::class, 'beranda']);
